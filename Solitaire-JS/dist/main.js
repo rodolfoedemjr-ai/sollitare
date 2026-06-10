fa45465fd01f0df1cdc61edcd6ed2dd2c9724f00
Jive Montrial A. Espina <montreal1240@gmail.com>
2026-06-10 10:16:52 +0800

    Add files via upload

---
+++ Solitaire-JS/dist/main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: false,
+      enableRemoteModule: false
+    }
+  })
+
+  win.loadFile(path.join(__dirname, 'index.html'))
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
